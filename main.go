063695a87640c7e876ff4cd7939e62583524aefb
JustFoxx <justfoxxo@outlook.com>
2021-10-15 22:48:38 +0200

    Another test (dont run)

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	install "FoxxoOS/installation"
 	s "FoxxoOS/main_server"
+	"fmt"
+	"os/exec"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	err := exec.Command("bash", "-c", "firefox --new-tab http://localhost:8080")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	install.Partitioning()
 
-	app := fiber.New(fiber.Config{
+	app := fiber.New(fiber.Config{s
 		AppName: "Foxxo OS",
 	})
 
