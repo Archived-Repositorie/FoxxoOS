107b61a277db9251baef0b973275bc76286844c1
JustFoxx <justfoxxo@outlook.com>
2021-11-03 18:39:35 +0100

    repaired problem with electron

--- start.sh
+++ start.sh
@@ -1,9 +1,13 @@
+#!/usr/bin/env bash
 #run script using command `bash`
 
 nix-env -iA nixos.go
+nix-env -iA electron
 
 git pull
 
 go build
+
+cp $(which electron) ./foxxoos/vendor/electron-linux-amd64/electron
 
 sudo ./FoxxoOS
